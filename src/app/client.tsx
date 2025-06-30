e0f615613154aef18ea3c1f907c2e939baf273b3
vishnuvardan2004 <vishnuvardan2004@gmail.com>
2025-06-30 22:24:21 +0530

    03: trpc-setup

---
+++ src/app/client.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+
+import {useTRPC } from "@/trpc/client";
+import {useSuspenseQuery} from "@tanstack/react-query";
+
+export const Client= () =>{
+    const trpc = useTRPC();
+    const {data} = useSuspenseQuery(trpc.createAI.queryOptions({text : "Antonio PERFECT"}));
+
+    return (
+        <div>
+        {JSON.stringify(data)}
+        </div>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import {getQueryClient,trpc} from "@/trpc/server";
+import {dehydrate , HydrationBoundary} from "@tanstack/react-query";
+import { Suspense } from "react";
+import {Client} from "./client";
+
 const page = async () => {
+const queryClient = getQueryClient();
+ void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Antonio PREFETCH"}));
+
   return (
-    <div>
-      jai sri ram 
-    </div>
+    <HydrationBoundary state ={dehydrate(queryClient)}>
+    <Suspense fallback={<p>loading....</p>}>  
+      <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
 
